85e988a079e752171fc646fcbf6f29f1c8947dea
Jair Tarrillo <jairtarrillo@MacBook-Pro-de-Jair.local>
2026-09-25 18:30:20 -0500

    Update Adult Swim to Latin American Spanish Astra feed (a01l/a0e6)

--- api/_core.js
+++ api/_core.js
@@ -90,7 +90,7 @@ export const CHANNELS = [
   { chno: 93, name: "Nickelodeon (HD - 16ms)", group: "Infantil", logo: "https://upload.wikimedia.org/wikipedia/commons/7/7a/Nickelodeon_2009_logo.svg", directUrl: "http://190.93.224.43/NICK/index.m3u8" },
   { chno: 94, name: "Discovery Kids (HD - 16ms)", group: "Infantil", logo: "https://upload.wikimedia.org/wikipedia/commons/e/e5/Discovery_Kids_2016.svg", directUrl: "http://190.93.224.43/DISCOVERY-KIDS/index.m3u8" },
   { chno: 95, name: "Disney Jr (HD 720p)", group: "Infantil", logo: "https://upload.wikimedia.org/wikipedia/commons/5/52/Disney_Junior_2011_logo.svg", directUrl: "http://181.78.14.26:4000/play/a073/index.m3u8" },
-  { chno: 96, name: "Adult Swim Latinoamérica (HD 720p)", group: "Series y Peliculas", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Adult_Swim_2003_logo.svg/960px-Adult_Swim_2003_logo.svg.png", slug: "adult-swim" },
+  { chno: 96, name: "Adult Swim Latinoamérica (HD - Español)", group: "Series y Peliculas", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Adult_Swim_2003_logo.svg/960px-Adult_Swim_2003_logo.svg.png", slug: "adult-swim" },
 
   // ── Cultura y Variedades ───────────────────────────────────────────────────
   { chno: 100, name: "Discovery Home & Health (H&H) (HD 1080p)", group: "Cultura", logo: "https://upload.wikimedia.org/wikipedia/commons/b/b3/Discovery_Home_%26_Health_logo.png", directUrl: "http://190.61.90.17:40000/play/a0hw/index.m3u8" },
@@ -140,12 +140,14 @@ export const DIRECT_HLS_MAP = {
     "http://23.237.104.106:8080/USA_PARAMOUNT_NETWORK/index.m3u8",
   ],
   "adult-swim": [
-    "http://168.197.104.22/ADULT_SWIM/index.m3u8",
-    "http://45.162.64.114/ADULT_SWIM/index.m3u8",
+    "http://45.185.163.75:8000/play/a01l/index.m3u8",
+    "http://45.185.163.75:8000/play/a0e6/index.m3u8",
+    "http://190.61.90.17:40000/play/a051/index.m3u8",
   ],
   adultswim: [
-    "http://168.197.104.22/ADULT_SWIM/index.m3u8",
-    "http://45.162.64.114/ADULT_SWIM/index.m3u8",
+    "http://45.185.163.75:8000/play/a01l/index.m3u8",
+    "http://45.185.163.75:8000/play/a0e6/index.m3u8",
+    "http://190.61.90.17:40000/play/a051/index.m3u8",
   ],
 };
 
